39518521e799a5d9e67dbd3ab00f1978b0797a01
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-09-10 09:40:29 +0530

    calc file added

---
+++ src/main/java/salesforce/cicd/MyCalc.java
@@ -0,0 +1,20 @@
+package salesforce.cicd;
+
+public class MyCalc {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is :"+calc.sum(20, 10));
+		System.out.println("Diff is :"+calc.diff(20, 10));
+	}
+}
